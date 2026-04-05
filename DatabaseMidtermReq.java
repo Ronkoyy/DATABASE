7022ef1ba44a5644d190aa2f358a96d623750975
Eron Moquete <eronmoquete4@gmail.com>
2026-04-05 03:25:15 +0100

    Started the project

---
+++ DatabaseMidtermReq.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.databasemidtermreq;
+
+/**
+ *
+ * @author eronm
+ */
+public class DatabaseMidtermReq {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
