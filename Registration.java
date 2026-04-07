297aeea7c69a78f64cf05db99add5627c9201744
Eron Moquete <eronmoquete4@gmail.com>
2026-04-07 21:25:22 +0800

    Renamed the Jframe Panels

--- Registration.java
+++ Registration.java
@@ -28,109 +28,109 @@ public class Registration extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel2 = new javax.swing.JPanel();
-        Rigester = new javax.swing.JPanel();
-        jPanel3 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        Name1 = new javax.swing.JLabel();
-        nameReg = new javax.swing.JTextField();
-        jLabel2 = new javax.swing.JLabel();
-        usernameReg = new javax.swing.JTextField();
+        Background = new javax.swing.JPanel();
+        RigesterWindow = new javax.swing.JPanel();
+        windowPane = new javax.swing.JPanel();
+        Register = new javax.swing.JLabel();
+        NameRegister = new javax.swing.JLabel();
+        nameRegField = new javax.swing.JTextField();
+        usernameRegister = new javax.swing.JLabel();
+        usernameRegField = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jPanel2.setBackground(new java.awt.Color(210, 230, 255));
+        Background.setBackground(new java.awt.Color(210, 230, 255));
 
-        Rigester.setBackground(new java.awt.Color(245, 235, 220));
-        Rigester.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 2));
+        RigesterWindow.setBackground(new java.awt.Color(245, 235, 220));
+        RigesterWindow.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 2));
 
-        jPanel3.setBackground(new java.awt.Color(110, 190, 180));
+        windowPane.setBackground(new java.awt.Color(110, 190, 180));
 
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout windowPaneLayout = new javax.swing.GroupLayout(windowPane);
+        windowPane.setLayout(windowPaneLayout);
+        windowPaneLayout.setHorizontalGroup(
+            windowPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 364, Short.MAX_VALUE)
         );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        windowPaneLayout.setVerticalGroup(
+            windowPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 12, Short.MAX_VALUE)
         );
 
-        jLabel1.setFont(new java.awt.Font("Retro Gaming", 0, 18)); // NOI18N
-        jLabel1.setText("Rigester");
+        Register.setFont(new java.awt.Font("Retro Gaming", 0, 18)); // NOI18N
+        Register.setText("Rigester");
 
-        Name1.setFont(new java.awt.Font("Retro Gaming", 0, 12)); // NOI18N
-        Name1.setText("Name");
+        NameRegister.setFont(new java.awt.Font("Retro Gaming", 0, 12)); // NOI18N
+        NameRegister.setText("Name");
 
-        nameReg.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        nameReg.addActionListener(this::nameRegActionPerformed);
+        nameRegField.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        nameRegField.addActionListener(this::nameRegFieldActionPerformed);
 
-        jLabel2.setFont(new java.awt.Font("Retro Gaming", 0, 12)); // NOI18N
-        jLabel2.setText("Username");
+        usernameRegister.setFont(new java.awt.Font("Retro Gaming", 0, 12)); // NOI18N
+        usernameRegister.setText("Username");
 
-        usernameReg.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        usernameRegField.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
-        javax.swing.GroupLayout RigesterLayout = new javax.swing.GroupLayout(Rigester);
-        Rigester.setLayout(RigesterLayout);
-        RigesterLayout.setHorizontalGroup(
-            RigesterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, RigesterLayout.createSequentialGroup()
+        javax.swing.GroupLayout RigesterWindowLayout = new javax.swing.GroupLayout(RigesterWindow);
+        RigesterWindow.setLayout(RigesterWindowLayout);
+        RigesterWindowLayout.setHorizontalGroup(
+            RigesterWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(windowPane, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, RigesterWindowLayout.createSequentialGroup()
                 .addGap(31, 31, 31)
-                .addGroup(RigesterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(Name1)
-                    .addComponent(jLabel2))
+                .addGroup(RigesterWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(NameRegister)
+                    .addComponent(usernameRegister))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(RigesterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1)
-                    .addComponent(usernameReg, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(nameReg, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(RigesterWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(Register)
+                    .addComponent(usernameRegField, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(nameRegField, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(49, 49, 49))
         );
-        RigesterLayout.setVerticalGroup(
-            RigesterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(RigesterLayout.createSequentialGroup()
-                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        RigesterWindowLayout.setVerticalGroup(
+            RigesterWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(RigesterWindowLayout.createSequentialGroup()
+                .addComponent(windowPane, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(Register, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addGroup(RigesterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(Name1)
-                    .addComponent(nameReg, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(RigesterWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(NameRegister)
+                    .addComponent(nameRegField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addGroup(RigesterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(usernameReg, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(RigesterWindowLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(usernameRegister)
+                    .addComponent(usernameRegField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(0, 121, Short.MAX_VALUE))
         );
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
+        javax.swing.GroupLayout BackgroundLayout = new javax.swing.GroupLayout(Background);
+        Background.setLayout(BackgroundLayout);
+        BackgroundLayout.setHorizontalGroup(
+            BackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(BackgroundLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(Rigester, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(RigesterWindow, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+        BackgroundLayout.setVerticalGroup(
+            BackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, BackgroundLayout.createSequentialGroup()
                 .addContainerGap(18, Short.MAX_VALUE)
-                .addComponent(Rigester, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(RigesterWindow, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(14, 14, 14))
         );
 
-        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 380, 270));
+        getContentPane().add(Background, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 380, 270));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void nameRegActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nameRegActionPerformed
+    private void nameRegFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nameRegFieldActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_nameRegActionPerformed
+    }//GEN-LAST:event_nameRegFieldActionPerformed
 
     /**
      * @param args the command line arguments
@@ -158,13 +158,13 @@ public class Registration extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel Name1;
-    private javax.swing.JPanel Rigester;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
-    private javax.swing.JTextField nameReg;
-    private javax.swing.JTextField usernameReg;
+    private javax.swing.JPanel Background;
+    private javax.swing.JLabel NameRegister;
+    private javax.swing.JLabel Register;
+    private javax.swing.JPanel RigesterWindow;
+    private javax.swing.JTextField nameRegField;
+    private javax.swing.JTextField usernameRegField;
+    private javax.swing.JLabel usernameRegister;
+    private javax.swing.JPanel windowPane;
     // End of variables declaration//GEN-END:variables
 }
