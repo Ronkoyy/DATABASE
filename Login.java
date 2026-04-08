97de1b77fe085386e2847f32a2f348c68e891927
Eron Moquete <eronmoquete4@gmail.com>
2026-04-08 19:48:35 +0800

    Added the log in logic

--- Login.java
+++ Login.java
@@ -8,6 +8,11 @@ package com.mycompany.databasemidtermreq;
  *
  * @author eronm
  */
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
 public class Login extends javax.swing.JFrame {
     
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(Login.class.getName());
@@ -35,10 +40,10 @@ public class Login extends javax.swing.JFrame {
         usernameRegister = new javax.swing.JLabel();
         usernameRegField = new javax.swing.JTextField();
         PasswordReg = new javax.swing.JLabel();
-        jPasswordField1 = new javax.swing.JPasswordField();
+        PassLogin = new javax.swing.JPasswordField();
         LoginBut = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
         Background = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
 
@@ -79,9 +84,9 @@ public class Login extends javax.swing.JFrame {
         PasswordReg.setText("Password");
         LoginWindow.add(PasswordReg, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 170, -1, -1));
 
-        jPasswordField1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        jPasswordField1.addActionListener(this::jPasswordField1ActionPerformed);
-        LoginWindow.add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 160, 189, 40));
+        PassLogin.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        PassLogin.addActionListener(this::PassLoginActionPerformed);
+        LoginWindow.add(PassLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 160, 189, 40));
 
         LoginBut.setBackground(new java.awt.Color(150, 220, 150));
         LoginBut.setFont(new java.awt.Font("Fixedsys Excelsior 3.01", 1, 24)); // NOI18N
@@ -90,21 +95,20 @@ public class Login extends javax.swing.JFrame {
         LoginBut.addActionListener(this::LoginButActionPerformed);
         LoginWindow.add(LoginBut, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 230, 120, 40));
 
-        jButton1.setBackground(new java.awt.Color(245, 235, 220));
-        jButton1.setFont(new java.awt.Font("Fixedsys Excelsior 3.01", 0, 12)); // NOI18N
-        jButton1.setText("Don't have an account?");
-        jButton1.setBorder(null);
-        jButton1.addActionListener(this::jButton1ActionPerformed);
-        LoginWindow.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 280, 150, -1));
-
         jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/retro.png"))); // NOI18N
-        LoginWindow.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 120, -1, -1));
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/key (1).png"))); // NOI18N
+        LoginWindow.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 170, -1, -1));
+
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/retro.png"))); // NOI18N
+        LoginWindow.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 120, -1, -1));
 
         getContentPane().add(LoginWindow, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, 590, 370));
 
         Background.setBackground(new java.awt.Color(210, 230, 255));
         Background.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/bg.jpg"))); // NOI18N
         Background.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(-50, -30, 720, 470));
 
         getContentPane().add(Background, new org.netbeans.lib.awtextra.AbsoluteConstraints(-40, -20, 670, 440));
@@ -117,20 +121,53 @@ public class Login extends javax.swing.JFrame {
     }//GEN-LAST:event_usernameRegFieldActionPerformed
 
     private void LoginButActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_LoginButActionPerformed
-        // TODO add your handling code here:
+     String user = usernameRegField.getText();
+     String pass = String.valueOf(PassLogin.getPassword());
+     
+     if(user.isEmpty()|| pass.isEmpty()){
+         JOptionPane.showMessageDialog(this, "Username and Password cannot be empty");
+         return;
+     }
+     try{
+         Connection conn = DBConnection.connectDB();
+         
+         String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+         PreparedStatement pst = conn.prepareStatement(sql);
+         pst.setString(1, user);
+         pst.setString(2, pass);
+         
+         ResultSet rs = pst.executeQuery();
+         
+        if (rs.next()) {
+        // SUCCESS: Found a matching user!
+        String name = rs.getString("fullName"); // Grabbing the name from the DB
+        JOptionPane.showMessageDialog(this, "Login Successful! Welcome, " + name);
+        
+        // 4. Transition to Dashboard
+        new Dashboard().setVisible(true);
+        this.dispose(); // Close the login window
+        
+    } else {
+        // FAIL: No match found
+        JOptionPane.showMessageDialog(this, "Invalid Username or Password", "Login Failed", JOptionPane.ERROR_MESSAGE);
+    }
+    
+    conn.close();
+    
+} catch (Exception e) {
+    JOptionPane.showMessageDialog(this, "Database Error: " + e.getMessage());
+}
+     
+     
     }//GEN-LAST:event_LoginButActionPerformed
 
-    private void jPasswordField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jPasswordField1ActionPerformed
+    private void PassLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_PassLoginActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jPasswordField1ActionPerformed
+    }//GEN-LAST:event_PassLoginActionPerformed
 
     private void controlwinActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_controlwinActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_controlwinActionPerformed
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -162,12 +199,12 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JLabel LOGIN;
     private javax.swing.JButton LoginBut;
     private javax.swing.JPanel LoginWindow;
+    private javax.swing.JPasswordField PassLogin;
     private javax.swing.JLabel PasswordReg;
     private javax.swing.JButton controlwin;
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JPasswordField jPasswordField1;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JTextField usernameRegField;
     private javax.swing.JLabel usernameRegister;
     private javax.swing.JPanel windowPane;
